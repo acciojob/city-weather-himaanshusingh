3a0b153e7d1431282a92f98e6c7aa8c526be946f
Himanshu Singh <himanshusingh1k@gmail.com>
2026-01-21 10:16:45 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,59 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
+import "regenerator-runtime/runtime";
 
 const App = () => {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+
+  function kelvinToFahrenheit(kelvin) {
+    return Math.round(((kelvin - 273.15) * 9) / 5 + 32);
+  }
+
+  function handleChange(e) {
+    setCity(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const API_KEY = "e467712b257e418838be97cc881a71de";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    (async function () {
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setWeather(data);
+        setCity("");
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={handleSubmit}>
+        <h1>Weather App</h1>
+        <input
+          className="search"
+          onChange={handleChange}
+          value={city}
+          placeholder="Enter a city"
+        />
+        {weather && (
+          <div className="weather">
+            <h1>{weather.name}</h1>
+            <h2>{kelvinToFahrenheit(weather.main.temp)} °F</h2>
+            <p>{weather.weather[0].description}</p>
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt="weather icon"
+            />
+          </div>
+        )}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
